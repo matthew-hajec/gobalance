c0889b7c47ed5121ff7f22250eaf2b82ee4e26cb
matthajec <matthew.hajec02@gmail.com>
2022-09-20 20:47:46 -0400

    reduced unnecessary exports

--- gobalance.go
+++ gobalance.go
@@ -7,46 +7,46 @@ import (
 	"sync"
 )
 
-type Server struct {
+type server struct {
 	URL   string
 	Up    bool
 	Proxy *httputil.ReverseProxy
 }
 
-type ServerPool struct {
+type serverPool struct {
 	mu      sync.Mutex
-	Servers []*Server
+	servers []*server
 	Offset  int
 }
 
-type GoBalancerInstance struct {
-	Pool *ServerPool
+type goBalancerInstance struct {
+	Pool *serverPool
 }
 
-func CreateGoBalancer() *GoBalancerInstance {
-	return &GoBalancerInstance{
-		Pool: &ServerPool{
-			Servers: []*Server{},
+func CreateGoBalancer() *goBalancerInstance {
+	return &goBalancerInstance{
+		Pool: &serverPool{
+			servers: []*server{},
 			Offset:  0,
 		},
 	}
 }
 
-func (g *GoBalancerInstance) getCurrentProxy() *httputil.ReverseProxy {
+func (g *goBalancerInstance) getCurrentProxy() *httputil.ReverseProxy {
 	g.Pool.mu.Lock()
 	defer g.Pool.mu.Unlock()
 
-	g.Pool.Offset = (g.Pool.Offset + 1) % len(g.Pool.Servers)
+	g.Pool.Offset = (g.Pool.Offset + 1) % len(g.Pool.servers)
 
-	return g.Pool.Servers[g.Pool.Offset].Proxy
+	return g.Pool.servers[g.Pool.Offset].Proxy
 }
 
-func (g *GoBalancerInstance) balanceRequest(w http.ResponseWriter, r *http.Request) {
+func (g *goBalancerInstance) balanceRequest(w http.ResponseWriter, r *http.Request) {
 	proxy := g.getCurrentProxy()
 	proxy.ServeHTTP(w, r)
 }
 
-func (g *GoBalancerInstance) Start(addr string) {
+func (g *goBalancerInstance) Start(addr string) {
 	http.HandleFunc("/", g.balanceRequest)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
